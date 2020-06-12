a402b9f281e9ba75601ef5086b612aa0239ce6b2
Jornwer <57893366+Jornwer@users.noreply.github.com>
2020-06-12 16:09:16 +0300

    Added fullscreen mode, added in-game method to quit program

--- src/game.py
+++ src/game.py
@@ -6,10 +6,11 @@ class Game:
     # TODO: Change cursor
     # TODO: Change icon : pygame.display.set_icon
     # TODO: Set font
-    # TODO: pygame.display.toggle_fullscreen
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1280, 720))  # get the screen
+        infos = pygame.display.Info()
+        screen_width, screen_height = infos.current_w, infos.current_h
+        self.screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)  # get the screen
         pygame.display.set_caption("Ninja")
         self.bg = 11, 102, 32
         self.time = 1.0  # Adjust time speed
@@ -21,6 +22,9 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:   # leave game by pressing escape
+                    if event.key == pygame.K_ESCAPE:
+                        sys.exit()
                 # On every event
 
             # After  every event
